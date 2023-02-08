6390b3a555e3d3fa2b5f381da4633c43c699bf31
Bert Gollnick <bert.gollnick@posteo.net>
2023-02-08 06:26:37 +0100

    clustering capstone added

---
+++ 60_Clustering/capstone_clustering.py
@@ -0,0 +1,6 @@
+#%% work with dataset "Credit Card Customer Data"
+# source: https://www.kaggle.com/datasets/aryashah2k/credit-card-customer-data
+
+# stored in subfolder data as "Credit Card Customer Data.csv"
+
+#%% Task: perform clustering based on an algorithm of your choice
